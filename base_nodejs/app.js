6306804cac5abc0740f234c664b55b04db644d7c
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2021-12-20 10:20:26 -0400

    tabla del 5

--- base_nodejs/app.js
+++ base_nodejs/app.js
@@ -1,4 +1,7 @@
 console.clear();
+console.log('============================');
+console.log('Tabla del 5');
+console.log('============================');
 const base = 5;
 
 for(let i = 1; i<=12; i++){
